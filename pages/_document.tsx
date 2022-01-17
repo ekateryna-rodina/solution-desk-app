c9103665626596f416af5d1a918a0fc72faf7476
ekateryna-rodina <work.rodina@gmail.com>
2022-01-17 08:00:02 -0500

    google font is added

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-page-custom-font */
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
