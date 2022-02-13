3482e2ced113a32284e53436871b0c927d97bd05
ekateryna-rodina <work.rodina@gmail.com>
2022-02-13 14:04:22 -0500

    TK-0013 ok icon is added

--- src/components/UserInfoActions/UserInfoActions.tsx
+++ src/components/UserInfoActions/UserInfoActions.tsx
@@ -1,23 +1,34 @@
 import React from "react";
-import { useAppDispatch } from "../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { enableEditMode } from "../../features/userInfo/userInfo-slice";
 import DeleteIcon from "../icons/DeleteIcon";
 import EditIcon from "../icons/EditIcon";
+import OkIcon from "../icons/OkIcon";
 
 const UserInfoActions = () => {
   const dispatch = useAppDispatch();
   const onEditModeHandler = () => {
-    dispatch(enableEditMode());
+    if (isEditMode) {
+    } else {
+      dispatch(enableEditMode());
+    }
   };
+  const { isEditMode } = useAppSelector((state) => state.userInfo);
   return (
     <div className="absolute top-4 left-0">
       <div className="flex flex-row justify-start items-center gap-2 px-4">
-        <button onClick={onEditModeHandler}>
-          <EditIcon stroke={"stroke-blueExtend/50"} />
-        </button>
         <button>
           <DeleteIcon fill={"fill-blueExtend/50"} />
         </button>
+        {isEditMode ? (
+          <button onClick={onEditModeHandler}>
+            <OkIcon fill={"fill-emerald-500"} />
+          </button>
+        ) : (
+          <button onClick={onEditModeHandler}>
+            <EditIcon stroke={"stroke-blueExtend/50"} />
+          </button>
+        )}
       </div>
     </div>
   );
